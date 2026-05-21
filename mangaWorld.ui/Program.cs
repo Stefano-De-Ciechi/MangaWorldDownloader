2928a3aba5fb3c12cd3c599b084c475f84def4c6
stefano.deciechi <stefano.deciechi@apkappa.it>
2026-05-21 15:53:14 +0200

    first prototype of scraper ui in ImGui.NET

---
+++ mangaWorld.ui/Program.cs
@@ -0,0 +1,69 @@
+﻿using Silk.NET.Input;
+using Silk.NET.OpenGL;
+using Silk.NET.OpenGL.Extensions.ImGui;
+using Silk.NET.Windowing;
+
+namespace mangaWorld.ui;
+
+public static class Program
+{
+    private static IWindow? _window;
+    private static GL? _gl;
+    private static ImGuiController? _controller;
+
+    private static Renderer? _appRenderer;
+    
+    static void Main(string[] args)
+    {
+        // create the main rendering window
+        var options = WindowOptions.Default;
+        options.Size = new Silk.NET.Maths.Vector2D<int>(1280, 720);
+        options.Title = "MangaWorld Scraper / Downloader";
+        
+        _window = Window.Create(options);
+
+        // append rendering window Event Handlers
+        _window.Load += OnLoad;
+        _window.Render += OnRender;
+        _window.Closing += OnClose;
+
+        _window.Run();
+    }
+
+    private static void OnLoad()
+    {
+        // get OpenGL context and window inputs
+        _gl = _window.CreateOpenGL();
+        var inputContext = _window?.CreateInput();
+
+        // initialize ImGuiController
+        _controller = new ImGuiController(_gl, _window, inputContext);
+        
+        // initialize my custom render logic
+        _appRenderer = new Renderer();
+    }
+
+    // executed every frame
+    private static void OnRender(double deltaTime)
+    {
+        // tell ImGui that a new frame is being rendered
+        _controller?.Update((float)deltaTime);
+
+        // draw my custom render logic here
+        _appRenderer?.Render();
+
+        // clear screen and render ImGui
+        _gl?.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        _gl?.Clear((uint) ClearBufferMask.ColorBufferBit);
+
+        // draw ImGui frame to the screen
+        _controller?.Render();
+    }
+
+    // resource cleanup
+    private static void OnClose()
+    {
+        _controller?.Dispose();
+        _gl?.Dispose();
+    }
+}
---
+++ mangaWorld.ui/Renderer.cs
@@ -0,0 +1,138 @@
+﻿using System.Numerics;
+using ImGuiNET;
+using MangaWorld.Core;
+using MangaWorld.Scraper;
+
+namespace mangaWorld.ui;
+
+public class Renderer
+{
+    private enum AppState
+    {
+        ToolSelector,
+        Scraper,
+        Downloader,
+    }
+    
+    private AppState _currentState =  AppState.ToolSelector;
+    
+    private string _mangaUrl = "";
+    private bool _isScaperWorking = false;
+    private string _scraperStatusMessage = "Ready to scrape";
+    
+    // main Render method (entry point)
+    public void Render()
+    {
+        var displaySize = ImGui.GetIO().DisplaySize;
+        
+        ImGui.SetNextWindowPos(Vector2.Zero);
+        ImGui.SetNextWindowSize(displaySize);
+        
+        var flags = 
+            ImGuiWindowFlags.NoCollapse | 
+            ImGuiWindowFlags.NoMove | 
+            ImGuiWindowFlags.NoResize | 
+            ImGuiWindowFlags.NoTitleBar;
+
+        ImGui.Begin("MangaWorld Scraper / Downloader", flags);
+
+        switch (_currentState)
+        {
+            case AppState.ToolSelector:
+                RenderToolSelector();
+                break;
+            case AppState.Scraper:
+                RenderScraper();
+                break;
+            case AppState.Downloader:
+                RenderDownloader();
+                break;
+            default:
+                return;
+        }
+        
+        ImGui.End();
+        
+        //ImGui.ShowMetricsWindow();
+    }
+    
+    private void RenderToolSelector()
+    {
+        var wSize = ImGui.GetWindowSize();
+        
+        ImGui.SetCursorScreenPos(new Vector2(wSize.X / 2 - 25, wSize.Y / 2 - 100));
+        ImGui.Text("Select Tool:");
+
+        ImGui.SetCursorScreenPos(new Vector2(wSize.X / 2 - 50, wSize.Y / 2 - 50));
+        if (ImGui.Button("Manga Scraper", new Vector2(200, 40)))
+            _currentState = AppState.Scraper;
+
+        ImGui.SetCursorScreenPos(new Vector2(wSize.X / 2 - 50, wSize.Y / 2));
+        if (ImGui.Button("Manga Downloader", new Vector2(200, 40)))
+            _currentState = AppState.Downloader;
+    }
+
+    private void RenderScraper()
+    {
+        if (ImGui.ArrowButton("##LeftArrow", ImGuiDir.Left)) _currentState = AppState.ToolSelector;
+        
+        ImGui.InputText("Manga URL", ref _mangaUrl, 2048);
+        
+        ImGui.SameLine();
+        if (ImGui.Button("Paste url from System Clipboard"))
+        {
+            _mangaUrl = ImGui.GetClipboardText();
+        }
+        
+        ImGui.SameLine();
+        if (ImGui.Button("Clear url"))
+        {
+            _mangaUrl = "";
+        }
+        
+        //if (_isScaperWorking) ImGui.BeginDisabled();
+
+        if (ImGui.Button("Start Scraping"))
+        {
+            StartBackgroundScraping();
+        }
+        
+        ImGui.SameLine();
+        ImGui.Text($"Scraper status: {_scraperStatusMessage}");
+        
+        //if (!_isScaperWorking) ImGui.EndDisabled();
+    }
+
+    private void StartBackgroundScraping()
+    {
+        if (string.IsNullOrWhiteSpace(_mangaUrl)) return;
+        
+        _isScaperWorking = true;
+        _scraperStatusMessage = "Scraper started ...";
+        
+        Task.Run(async () => 
+        {
+            try
+            {
+                var infoScraper = new InfoScraper(_mangaUrl);
+                var manga = infoScraper.Scrape();
+
+                await MangaSerializer.Serialize(manga);
+                _scraperStatusMessage = $"Scraper finished (tot. html pages requested: {infoScraper.TotalPagesRequested})";
+            }
+            catch (Exception e)
+            {
+                _scraperStatusMessage = $"Error: {e.Message}";
+            }
+            finally
+            {
+                _isScaperWorking = false;
+            }
+        });
+    }
+
+    private void RenderDownloader()
+    {
+        if (ImGui.ArrowButton("##LeftArrow", ImGuiDir.Left)) _currentState = AppState.ToolSelector;
+    }
+}
