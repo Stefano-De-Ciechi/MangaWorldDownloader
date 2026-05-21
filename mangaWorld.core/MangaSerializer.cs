3bcf89437da302f314a9368c7d7b4f8692c63e86
stefano.deciechi <stefano.deciechi@apkappa.it>
2026-05-21 17:18:57 +0200

    added default data folders public properties

--- mangaWorld.core/MangaSerializer.cs
+++ mangaWorld.core/MangaSerializer.cs
@@ -8,13 +8,17 @@ public static class MangaSerializer
     public static string DocumentsRootPath { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
     private static string _dataFolder = Path.Combine(DocumentsRootPath, "MWD/Data");
     
+    public static string InfoFolder { get; private set; } = Path.Combine(_dataFolder, "Info");
+    public static string CoversFolder { get; private set; } = Path.Combine(_dataFolder, "Covers");
+    public static string DownloadsFolder { get; private set; } = Path.Combine(_dataFolder, "Downloads");
+    
     public static async Task Serialize(Manga manga)
     {
         //var fileName = $"../Data/info/{ manga.Name }-info.json";       // TODO make the path platform indepentent (and maybe cofigurable from a .env, .cfg or other configuration files)
-        var fileName = $"{Path.Combine(_dataFolder, $"Info/{manga.Name}-info.json")}";
+        var fileName = $"{Path.Combine(InfoFolder, $"{manga.Name}-info.json")}";
         var options = new JsonSerializerOptions { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
-        Directory.CreateDirectory(Path.Combine(_dataFolder, "Info"));
+        Directory.CreateDirectory(InfoFolder);
         await using FileStream createStream = File.Create(fileName);
         await JsonSerializer.SerializeAsync<Manga>(createStream, manga, options);
         //await createStream.DisposeAsync();    // tecnically not needed since there is the "using" statement
@@ -22,13 +26,13 @@ public static class MangaSerializer
         Console.WriteLine("finished scraping and writing to JSON file");
     }
 
-    public static async Task<Manga?> Deserialize(string fileName, string filePath = "../Data/info/")
+    public static async Task<Manga?> Deserialize(string fileName)
     {
         //var file = $"{ filePath }/{ fileName }";    // TODO add checks on the file format, name, existence etc.
-        var file = $"{Path.Combine(DocumentsRootPath, $"Downloads/{fileName}")}";
+        var file = $"{Path.Combine(InfoFolder, $"{fileName}-info.json")}";
         var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
         
-        using FileStream readStream = File.OpenRead(file);
+        await using FileStream readStream = File.OpenRead(file);
         Manga? manga = await JsonSerializer.DeserializeAsync<Manga>(readStream, options);
 
         return manga;
