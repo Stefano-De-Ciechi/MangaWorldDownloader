deef1ebf3a635efd6a397cfc85914130451f2fa1
stefano.deciechi <stefano.deciechi@apkappa.it>
2026-05-21 15:52:54 +0200

    changed default Data folder location to Documents

--- mangaWorld.core/MangaSerializer.cs
+++ mangaWorld.core/MangaSerializer.cs
@@ -2,14 +2,20 @@ using System.Text.Json;
 
 namespace MangaWorld.Core;
 
-public class MangaSerializer
+public static class MangaSerializer
 {
+
+    public static string DocumentsRootPath { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+    private static string _dataFolder = Path.Combine(DocumentsRootPath, "MWD/Data");
+    
     public static async Task Serialize(Manga manga)
     {
-        var fileName = $"../Data/info/{ manga.Name }-info.json";       // TODO make the path platform indepentent (and maybe cofigurable from a .env, .cfg or other configuration files)
+        //var fileName = $"../Data/info/{ manga.Name }-info.json";       // TODO make the path platform indepentent (and maybe cofigurable from a .env, .cfg or other configuration files)
+        var fileName = $"{Path.Combine(_dataFolder, $"Info/{manga.Name}-info.json")}";
         var options = new JsonSerializerOptions { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
-        using FileStream createStream = File.Create(fileName);
+        Directory.CreateDirectory(Path.Combine(_dataFolder, "Info"));
+        await using FileStream createStream = File.Create(fileName);
         await JsonSerializer.SerializeAsync<Manga>(createStream, manga, options);
         //await createStream.DisposeAsync();    // tecnically not needed since there is the "using" statement
 
@@ -18,7 +24,8 @@ public class MangaSerializer
 
     public static async Task<Manga?> Deserialize(string fileName, string filePath = "../Data/info/")
     {
-        var file = $"{ filePath }/{ fileName }";    // TODO add checks on the file format, name, existence etc.
+        //var file = $"{ filePath }/{ fileName }";    // TODO add checks on the file format, name, existence etc.
+        var file = $"{Path.Combine(DocumentsRootPath, $"Downloads/{fileName}")}";
         var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
         
         using FileStream readStream = File.OpenRead(file);
